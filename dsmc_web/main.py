bfc2c027251a846bf172c272363598bf4a7bba62
Pir Ate <atepir0@gmail.com>
2020-06-28 10:36:33 +0200

    [ERROR] Backend Error : Import local modules failed. Trying to correct

--- dsmc_web/main.py
+++ dsmc_web/main.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
-import os
+from dontstealmycode.dsmc import Code
 
 app = Flask(__name__)
 app.secret_key = b"macrohacks"
@@ -17,26 +17,39 @@ def upload_file():
 def uploads_file():
     if request.method == 'POST':
         f = request.files['file']
+        encryption = request.form.get('encrypt')
+        decryption = request.form.get('decrypt')
+        global todo
+        if encryption != "":
+            todo = 0
+        else:
+            todo = 1
+        global f1
+        f1 = request.form.get('f1')
+        global f2
+        f2 = request.form.get('f2')
         name = f.filename
+        global secured_name
         secured_name = secure_filename(name)
         final_name = UPLOAD_DIRECTORY + secured_name
-        download_path = UPLOAD_SHORT_DIR + secured_name
-        output_name = "encrypted_" + secured_name
-        folder_index = 0
-        for i in range(10000):
-            folder_index += 1
-            if not os.path.exists(UPLOAD_DIRECTORY + "temp" + str(i)):
-                os.mkdir(UPLOAD_DIRECTORY + "temp" + str(i))
-                break
-        f.save(UPLOAD_DIRECTORY + "temp" + str(folder_index - 1) + f.filename)
-        page = '<a href="/get_file?path='+download_path+'&name='+output_name+'">Telecharger le fichier</a>'
-        return page
+        f.save(final_name)
+        return render_template('download.html')
     else:
         return render_template('index.html')
 
-@app.route('/get_file', methods=['GET'])
+@app.route('/get_file')
 def get_fil():
-    if request.method == 'GET':
-        path = request.form.get('path')
-        name = request.form.get('name')
-        return send_file(path, as_attachment=True, attachment_filename=name)
+    global secured_name
+    received = UPLOAD_SHORT_DIR + secured_name
+    global f1, f2
+    codeObject = Code(received, f1, f2)
+    encrypted_name = "encrypted_" + secured_name
+    decrypted_name = "decrypted_" + secured_name
+    global todo
+    if todo == 1:
+        codeObject.encrypt(output=UPLOAD_SHORT_DIR + encrypted_name)
+        return send_file(UPLOAD_SHORT_DIR + encrypted_name, as_attachment=True, attachment_filename=encrypted_name)
+    else:
+        codeObject.decrypt(output=UPLOAD_SHORT_DIR + decrypted_name)
+        return send_file(UPLOAD_SHORT_DIR + decrypted_name, as_attachment=True, attachment_filename=decrypted_name)
+
